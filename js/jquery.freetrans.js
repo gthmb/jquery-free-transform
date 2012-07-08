bb722c8b25af19b37fe47dcae751c4fba347a997
Jonathan Greene <jon@greenethumb.com>
2012-07-08 17:16:39 -0400

    added settable rotation points!

--- js/jquery.freetrans.js
+++ js/jquery.freetrans.js
@@ -82,6 +82,7 @@
 			scalex: 1,
 			scaley: 1, 
 			angle: 0,
+			'rot-origin': '50% 100%',
 			divs: {}
 		}, options);
 		
@@ -318,6 +319,7 @@
 	
 	function _getBounds(sel) {
 		var bnds = {};
+
 		sel.find('.ft-scaler').each(function(indx) {
 			var handle = $(this),
 			off = handle.offset(),
@@ -364,6 +366,36 @@
 		return Point(pt.x * cos + pt.y * sin, pt.y * cos - pt.x * sin);
 	}
 	
+	function _getRotationPoint(sel) {
+		var data = sel.data('freetrans'), 
+		ror = data['rot-origin'], 
+		pt = Point(0,0);
+		
+		if(!ror) return pt;
+		
+		var arr = ror.split(' '), l = arr.length;
+		
+		if(!l) return pt;
+		
+		var val = parseInt(arr[0]), 
+		per = arr[0].indexOf('%') > -1,
+		ctrls = data.divs.controls,
+		dim = ctrls.width();
+
+		pt.x = ((per) ? val/100*dim : val) - dim/2;
+
+		if(l==1)  pt.y = pt.x;
+		else {
+			val = arr[1];
+			per = val.indexOf('%') > -1;
+			val = parseInt(val);	
+			dim = ctrls.height();
+			pt.y = ((per) ? val/100*dim : val) - dim/2;		
+		}
+
+		return pt;
+	}
+
 	function _matrixToCSS(m) {
 		return "matrix(" + m.a + "," + m.b + "," + m.c + "," + m.d + "," + m.tx + "," + m.ty + ")";
 	}
@@ -381,9 +413,10 @@
 		y = data.y,
 		sx = data.scalex,
 		sy = data.scaley,
-		mat = Matrix().rotate(radian).scale(sx, sy),
+		ror = data['rot-origin'];
+		mat = Matrix().rotate(radian, _getRotationPoint(sel)).scale(sx, sy),
 		tstr = _matrixToCSS(Matrix().rotate(radian));
-		
+
 		ctrls.css({
 			top: y + sel.height() * (1 - sy),
 			left: x + sel.width() * (1 - sx),
@@ -393,7 +426,12 @@
 			"-webkit-transform": tstr,
 			"-moz-transform": tstr,
 			"-o-transform": tstr,
-			"-ms-transform": tstr
+			"-ms-transform": tstr,
+			"transform-origin": ror,
+			"-webkit-transform-origin": ror,
+			"-moz-transform-origin": ror,
+			"-o-transform-origin": ror,
+			"-ms-transform-origin": ror
 		});
 		
 		tstr = _matrixToCSS(Matrix().rotate(-radian));
@@ -410,6 +448,7 @@
 		
 		tstr = _matrixToCSS(mat);
 		
+		// rotate and position
 		sel.css({
 			position: 'absolute',
 			top: y + sel.height() * (1 - sy) / 2,
